9556b16552554a37fbcfbc5f766055aaeaf927e2
Alexph7 <ph757@Fabia>
2023-01-13 00:06:15 -0300

    Trocar Nome de Classes
    Renomear Nomes da Classes Mais Claras

--- src/com/exercicios/LinhasTest.java
+++ src/com/exercicios/LinhasTest.java
@@ -11,13 +11,13 @@ public class LinhasTest {
 
     public static void main(String[] args) {
 
-        Linhas objExercicios = new Linhas();
+        Linhas objLinhas = new Linhas();
 
         JFrame app = new JFrame();
         app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        objExercicios.setBackground(Color.white);
-        app.add(objExercicios);
+        objLinhas.setBackground(Color.white);
+        app.add(objLinhas);
         app.setSize(500, 500);
         app.setVisible(true);
 
